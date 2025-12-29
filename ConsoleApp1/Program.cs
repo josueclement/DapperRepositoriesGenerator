9330630352d2ccacb5381abc9b852316f3623248
Josué Clément <josue.d.clement@gmail.com>
2025-12-29 14:03:09 +0100

    Fix bug in IService

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.IO;
 using System.Threading.Tasks;
 using Bogus;
 using DapperRepositoriesGenerator;
@@ -33,8 +35,9 @@ class Program
             "System.Data.SQLite.SQLiteFactory, System.Data.SQLite");
         
         // TestScriban();
-        Generate();
-        
+        // Generate();
+        await GenerateEventManager();
+
         // await TestInsert();
         // await TestSelectAll();
         // await TestSelectById();
@@ -62,6 +65,68 @@ class Program
         
         connection.ConnectionString = "Data Source=users.db";
         return connection;
+    }
+
+    static async Task GenerateEventManager()
+    {
+        var tables = new[]
+        {
+            new DbTable("Artist", ["Id", "CreationDate", "ModificationDate", "ParentId", "Name", "Role", "Hotel", "StayStartDate", "StayEndDate", "Contact", "TravelDetails", "Transport", "Parking", "Misc", "Contract", "Rider", "Menus", "Roadmap", "Suisa", "Remarks"]),
+            new DbTable("Car", ["Id", "CreationDate", "ModificationDate", "Name", "Description", "LicensePlate"]),
+            new DbTable("Location", ["Id", "CreationDate", "ModificationDate", "Name", "Description"]),
+            new DbTable("Staff", ["Id", "CreationDate", "ModificationDate", "Name", "Contact", "Description"]),
+            new DbTable("Volunteer", ["Id", "CreationDate", "ModificationDate", "Name", "Contact", "Description"])
+        };
+
+        var sqlGenerator = new SqlGenerator(new SqlGeneratorOptions());
+        var repositoryGenerator = new RepositoryGenerator(sqlGenerator, new RepositoryGeneratorOptions
+        {
+            RepositoryInterfaceNamespace =  "EventManager.Application.Interfaces.Repositories",
+            RepositoryNamespace = "EventManager.Infrastructure.Database.Repositories",
+            EntitiesNamespace =  "EventManager.Domain.Entities"
+        });
+        var serviceGenerator = new ServiceGenerator(new ServiceGeneratorOptions
+        {
+            ServiceInterfaceNamespace = "EventManager.Application.Interfaces.Services",
+            ServiceNamespace = "EventManager.Infrastructure.Database.Services",
+            RepositoryInterfaceNamespace = "EventManager.Application.Interfaces.Repositories",
+            EntitiesNamespace = "EventManager.Domain.Entities"
+        });
+        var entityGenerator = new EntityGenerator("EventManager.Domain.Entities");
+
+        var basePath = "/home/jo/Dev/EventManager/";
+        var repositoriesInterfacesPath = Path.Combine(basePath, "EventManager.Application", "Interfaces", "Repositories");
+        var servicesInterfacesPath = Path.Combine(basePath, "EventManager.Application", "Interfaces", "Services");
+        var repositoriesPath = Path.Combine(basePath, "EventManager.Infrastructure", "Database", "Repositories");
+        var servicesPath = Path.Combine(basePath, "EventManager.Infrastructure", "Database", "Services");
+        var entitiesPath = Path.Combine(basePath, "EventManager.Domain", "Entities");
+        var scriptsPath = Path.Combine(basePath, "EventManager.Infrastructure", "Database");
+        
+        var repositoryGenericInterface = repositoryGenerator.GenerateRepositoryGenericInterface();
+        await File.WriteAllTextAsync(Path.Combine(repositoriesInterfacesPath, "IRepository.cs"), repositoryGenericInterface);
+        var serviceGenericInterface = serviceGenerator.GenerateServiceGenericInterface();
+        await File.WriteAllTextAsync(Path.Combine(servicesInterfacesPath, "IService.cs"), serviceGenericInterface);
+        
+        foreach (var table in tables)
+        {
+            var tableName = table.TableName;
+            
+            var script = sqlGenerator.GenerateCreateTableScript(table);
+            await File.WriteAllTextAsync(Path.Combine(scriptsPath, tableName + ".sql"), script);
+            
+            var repositoryInterface = repositoryGenerator.GenerateRepositoryInterface(table);
+            await File.WriteAllTextAsync(Path.Combine(repositoriesInterfacesPath, $"I{tableName}Repository.cs"), repositoryInterface);
+            var repository = repositoryGenerator.GenerateRepository(table);
+            await File.WriteAllTextAsync(Path.Combine(repositoriesPath, $"{tableName}Repository.cs"), repository);
+            
+            var serviceInterface = serviceGenerator.GenerateServiceInterface(table);
+            await File.WriteAllTextAsync(Path.Combine(servicesInterfacesPath, $"I{tableName}Service.cs"), serviceInterface);
+            var service = serviceGenerator.GenerateService(table);
+            await File.WriteAllTextAsync(Path.Combine(servicesPath, $"{tableName}Service.cs"), service);
+            
+            var entity = entityGenerator.GenerateEntity(table);
+            await File.WriteAllTextAsync(Path.Combine(entitiesPath, $"{tableName}.cs"), entity);
+        }
     }
 
     static void Generate()
